4d97c98050817989b61e4e96d7cae310e4a5059c
johnlaschober <johnlaschober@gmail.com>
2018-03-22 15:09:11 -0500

    Working implementation
    This is working locally and on Azure although it has outdated references to all static JSON sites and the groupJSON site, so those need to be updated.
    
    Also all HTML / JavaScript was tossed into server.js in order to get this to work. A future user story might be to separate these again.

--- server.js
+++ server.js
@@ -1,68 +1,136 @@
-var request = require('request'); // Imports libraries
 var http = require('http');
+
+var request = require('request'); // Imports libraries
 var fs = require('fs');
 
 // Define URLs here
-var allURLS = ["https://csoftware.azurewebsites.net/groupinfo.json", "https://johnlaschobersoftwareengineering.azurewebsites.net/info.json", "https://cpsc440.azurewebsites.net/myInfo.json", "http://alisp18.azurewebsites.net/myinfo.json"];
+var allURLS = ["http://johnlaschobersoftwareengineering.azurewebsites.net/myInfo.json", "https://cpsc440.azurewebsites.net/myInfo.json"];
 // Where JSON objects will be stored
 var allJSON = [];
 
 var concattedJSON;
 var finalJSON = "no";
+var done = false;
 
-function UpdateJSON(createdResponse)
+var indexStack = [];
+
+for (i = 0; i < allURLS.length; i++)
 {
-	for (i = 0; i < allURLS.length; i++) // Initiates on server launch
+	indexStack.push(i);
+	request(allURLS[i], function (error, response, body) 
 	{
-		request(allURLS[i], function (error, response, body)
+		console.log(response.statusCode);
+		if (!error && response.statusCode == 200) 
 		{
-			if (!error && response.statusCode == 200) 
+			var iterator = indexStack[indexStack.length - 1];
+			indexStack.pop();
+			var importedJSON = JSON.parse(body);
+			allJSON.push(importedJSON);
+			var outputString = JSON.stringify(importedJSON, null, 4);
+			fs.writeFile("member" + iterator + ".json", outputString, 'utf-8', function (err)  // File writer for saving a json file, not done
 			{
-				var importedJSON = JSON.parse(body);
-				allJSON.push(importedJSON);
-				//console.log(allJSON.length);
-				if (allJSON.length == allURLS.length)
+				if (err) 
 				{
-					var concattedJSON = "";
-					concattedJSON = '{"members":[';
-					for (j = 0; j < allURLS.length; j++)
-					{
-						concattedJSON += JSON.stringify(allJSON[j]);
-						if (j != allURLS.length - 1)
-						{
-							concattedJSON += ',';
-						}
-					}
-					concattedJSON += ']}';
-					//console.log(concattedJSON);
-					finalJSON = JSON.parse(concattedJSON);
-					//console.log(finalJSON);
-					createdResponse.write(JSON.stringify(finalJSON, null, 4));
-					createdResponse.end(); // End of http stream, can no longer update website
-					
-					/*
-					var stringFinal = JSON.stringify(finalJSON, null, 4);
-					fs.writeFile("groupJSON.json", stringFinal,'utf-8', function(err)  // File writer for saving a json file, not done
-					{
-						if(err) 
-						{
-							return console.log(err);
-						}
-					});
-					*/
-					
+					return console.log(err);
 				}
-			}
-		});
-	} 
+				else
+				{
+					if (indexStack.length == 0)
+					{
+						var concattedJSON = "";
+						concattedJSON = '{"members":[';
+						for (j = 0; j < allJSON.length; j++)
+						{
+							concattedJSON += JSON.stringify(allJSON[j]);
+							if (j != allURLS.length - 1) 
+							{
+								concattedJSON += ',';
+							}
+						}
+						concattedJSON += ']}';
+						fs.writeFile("groupJSON.json", concattedJSON, 'utf-8', function (err)  // File writer for saving a json file, not done
+						{
+							if (err) 
+							{
+								return console.log(err);
+							}
+						});
+					}
+				}
+			});
+
+		}
+	});
 }
 
-var server = http.createServer(function(request, response)  // On user connect
-{
-	UpdateJSON(response); // Send respond to function because it's asynchronous
-    response.writeHead(200, {"Content-Type": "text/plain"}); 
+var liveHTML = '<head>\
+        <meta charset="UTF-8">\
+        <title>PROJECT: Klump</title>\
+        <script>\
+		function load()\
+		{\
+			try\
+			{\
+				var data = getJSON("https://jsonmergingtest.azurewebsites.net/groupJSON.json");\
+				data = JSON.parse(data);\
+				for (i = 0; i < 4; i++)\
+				{	\
+					document.getElementById(i).innerHTML = "First Name: " + data.members[i].FirstName + "<br>";\
+					document.getElementById(i).innerHTML += "Last Name: " + data.members[i].LastName + "<br>";\
+					document.getElementById(i).innerHTML += "Preferred Name: " + data.members[i].PreferredName + "<br>";\
+					document.getElementById(i).innerHTML += "Team Name: " + data.members[i].TeamName + "<br>";\
+					document.getElementById(i).innerHTML += "Seat Location: " + data.members[i].SeatLocation + "<br>";\
+					document.getElementById(i).innerHTML += "Roles: " + data.members[i].Roles + "<br>";\
+				}\
+				console.log(data.members);\
+			}\
+			catch (err)\
+			{\
+				console.log("groupJSON.json could not be accessed. " + err);\
+			}\
+			 function getJSON(url) \
+			 {\
+				var resp ;\
+				var xmlHttp ;\
+				resp  = "" ;\
+				xmlHttp = new XMLHttpRequest();\
+				if(xmlHttp != null)\
+				{\
+					xmlHttp.open( "GET", url, false );\
+					xmlHttp.send( null );\
+					resp = xmlHttp.responseText;\
+				}\
+				return resp ;\
+			}\
+		}\
+		</script>\
+	</head>\
+	<body onload="load()">\
+		<p id="0"></p>\
+		<p id="1"></p>\
+		<p id="2"></p>\
+		<p id="3"></p>\
+		<p id="4"></p>\
+		<p id="5"></p>\
+	</body>';
 
-	//var importedJSON = JSON.parse(fs.readFileSync('groupJSON.json', 'utf8'));    // Reading from input
+var server = http.createServer(function (request, response)  // On user connect
+{
+    //response.writeHead(200, { "Content-Type": "text/plain" });
+    try
+    {
+        var importedJSON = JSON.parse(fs.readFileSync('groupJSON.json', 'utf8'));    // Reading from input
+        //response.write(JSON.stringify(importedJSON, null, 4));
+		response.write(liveHTML);
+		response.end();
+    }
+    catch (err)
+    {
+		response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write("Something went wrong... " + err);
+		response.end();
+    }
+
 });
 
 var port = process.env.PORT || 80;
