a3643eb194e17f4f34f659094c0ab960b5a74a2d
johnlaschober <johnlaschober@gmail.com>
2018-04-05 10:54:41 -0500

    Added html pushing back to server.js

--- server.js
+++ server.js
@@ -31,6 +31,56 @@ function concatOtherGroupsJson(url)
 concatOtherGroupsJson("http://gwb-json-info.azurewebsites.net/");
 concatOtherGroupsJson("https://flamingos.azurewebsites.net/json");
 
+var liveHTML = '<head>\
+        <meta charset="UTF-8">\
+        <title>PROJECT: Klump</title>\
+        <script>\
+		function load()\
+		{\
+			var data = getJSON("http://gwb-json-info.azurewebsites.net/");\
+			data = JSON.parse(data);\
+			var storedPosition = "";\
+			for (i = 0; i < 6; i++)\
+			{	\
+				document.getElementById(i).innerHTML = "First Name: " + data.members[i].FirstName + "<br>";\
+				document.getElementById(i).innerHTML += "Last Name: " + data.members[i].LastName + "<br>";\
+				document.getElementById(i).innerHTML += "Preferred Name: " + data.members[i].PreferredName + "<br>";\
+				document.getElementById(i).innerHTML += "Team Name: " + data.members[i].TeamName + "<br>";\
+				document.getElementById(i).innerHTML += "Seat Location: " + data.members[i].SeatLocation + "<br>";\
+				document.getElementById(i).innerHTML += "Roles: " + data.members[i].Roles + "<br>";\
+				if (storedPosition > data.members[i].SeatLocation)\
+				{\
+					console.log(storedPosition + " greater than " + data.members[i].SeatLocation);\
+				}\
+				storedPosition = data.members[i].SeatLocation;\
+			}\
+			console.log(data.members);\
+			function getJSON(url) \
+			{\
+				var resp ;\
+				var xmlHttp ;\
+				resp = "" ;\
+				xmlHttp = new XMLHttpRequest();\
+				if(xmlHttp != null)\
+				{\
+					xmlHttp.open( "GET", url, false );\
+					xmlHttp.send( null );\
+					resp = xmlHttp.responseText;\
+				}\
+				return resp ;\
+			}\
+		}\
+		</script>\
+	</head>\
+	<body onload="load()">\
+		<p id="0"></p>\
+		<p id="1"></p>\
+		<p id="2"></p>\
+		<p id="3"></p>\
+		<p id="4"></p>\
+		<p id="5"></p>\
+	</body>';
+
 var server = http.createServer(function (request, response)  // On user connect
 {
     //response.writeHead(200, { "Content-Type": "text/plain" });
@@ -42,7 +92,7 @@ var server = http.createServer(function (request, response)  // On user connect
     {
         //var importedJSON = JSON.parse(fs.readFileSync('groupJSON.json', 'utf8'));    // Reading from input
 		//console.log(JSON.parse(fs.readFileSync('groupJSON.json', 'utf8')));
-        response.write(JSON.stringify(combined));
+        response.write(liveHTML);
 		try
 		{
 			var lastUpdated = fs.readFileSync('lastUpdated.txt', 'utf8');
