145a7d1b334ed44414ac4e57f84d21655632db5f
johnlaschober <johnlaschober@gmail.com>
2018-04-07 12:32:37 -0500

    Repo cleanup
    Got rid of any files / code that wasn't actively being used by the server

--- server.js
+++ server.js
@@ -1,65 +1,9 @@
 var http = require('http');
-var mergeJSON = require("merge-json");
 var request = require('request'); // Imports libraries
 var fs = require('fs');
-var express = require('express');
-var app = express();
-
-express.static("C:\Users\robob\Desktop\klump-gwb-test");
-app.use(express.static('public'));
-app.listen(5000);
-
-// Define URLs here
-var allURLS = ["https://johnlaschobersoftwareengineering.azurewebsites.net/myInfo.json", "https://softwareengineeringapp.azurewebsites.net/my-information.json", "https://korszulak-static.azurewebsites.net/myInfo.json", "http://csoftware.azurewebsites.net/groupinfo.json", "https://cpsc440.azurewebsites.net/myInfo.json", "https://alisp18.azurewebsites.net/myinfo.json"];
-// Where JSON objects will be stored
-var allJSON = [];
-var groupJSONs = [];
-
-var concattedJSON;
-var finalJSON = "no";
-var done = false;
-
-var indexStack = [];
-
-function concatOtherGroupsJson(url)
-{
-	request(url, function (error, response, body) 
-	{
-		if (!error && response.statusCode == 200) 
-		{
-			var importedJSON = JSON.parse(body);
-			groupJSONs.push(importedJSON);
-		}
-	});
-}
-
-function concatGroupJsons()
-{
-	concatOtherGroupsJson("http://gwb-json-info.azurewebsites.net/");
-	concatOtherGroupsJson("https://flamingos.azurewebsites.net/json");
-	fs.writeFile("lastUpdated.txt", Date(), 'utf-8', function (err) 
-	{ 
-		if (err) 
-		{ 
-			return console.log(err); 
-		} 
-	}); 
-	setTimeout(concatGroupJsons, 1000 * 60 * 30); // 30 minute refresh in milliseconds 
-}
-concatGroupJsons();
-
-
 
 var server = http.createServer(function (request, response)  // On user connect
 {
-    //response.writeHead(200, { "Content-Type": "text/plain" });
-	
-	//var combined = Object.assign(groupJSONs[0], groupJSONs[1]);
-	try
-	{
-		var combined = mergeJSON.merge(groupJSONs[0], groupJSONs[1]);
-	} catch (err) {}
-	
 	var liveHTML = '<head>\
         <meta charset="UTF-8">\
         <title>PROJECT: Klump</title>\
@@ -111,9 +55,6 @@ var server = http.createServer(function (request, response)  // On user connect
 		<p id="5"></p>\
 	</body>';
 	
-	
-	//console.log(liveHTML);
-	//console.log(JSON.parse(testData));
 	try
     {
         //var importedJSON = JSON.parse(fs.readFileSync('groupJSON.json', 'utf8'));    // Reading from input
